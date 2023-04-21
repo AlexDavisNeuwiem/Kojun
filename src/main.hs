7d42ae14b46a99744b9c4134b918668e34a6038e
LucasTruppel <lucasctruppelm@gmail.com>
2023-04-21 19:34:10 -0300

    Regioes definidas

--- src/main.hs
+++ src/main.hs
@@ -1,5 +1,24 @@
-criarMatriz :: Int -> [[Int]]
-criarMatriz n = [[0 | j <- [1..n]] | i <- [1..n]]
+matrizNumeros :: [[Int]]
+matrizNumeros = [[2,0,0,0,1,0],
+                 [0,0,0,3,0,0],
+                 [0,3,0,0,5,3],
+                 [0,0,0,0,0,0],
+                 [0,0,3,0,4,2],
+                 [0,0,0,0,0,0]]
+
+matrizRegioes :: [[Int]]
+matrizRegioes = [[0 ,0 ,1 ,1 ,1 ,2],
+                 [3 ,3 ,3 ,3 ,3 ,2],
+                 [4 ,5 ,5 ,5 ,3 ,6],
+                 [4 ,4 ,4 ,5 ,6 ,6],
+                 [7 ,7 ,8 ,9 ,9 ,9],
+                 [10,10,8 ,8 ,9 ,9]]
+
+tamanhoMatriz :: Int
+tamanhoMatriz = 6
+
+quantidadeRegioes :: Int
+quantidadeRegioes = 11
 
 imprimirMatriz :: [[Int]] -> IO String
 imprimirMatriz [] = return ""
@@ -7,7 +26,22 @@ imprimirMatriz (a:b) = do
     print a
     imprimirMatriz b
 
+definirRegioes :: [[Int]] -> Int -> Int -> [[(Int, Int)]]
+definirRegioes regioesMatriz quantidadeRegioes tamanhoMatriz =
+    let regioes = replicate quantidadeRegioes []
+    in atualizarRegioes regioesMatriz regioes tamanhoMatriz
+
+atualizarRegioes :: [[Int]] -> [[(Int, Int)]] -> Int -> [[(Int, Int)]]
+atualizarRegioes regioesMatriz regioes tamanhoMatriz =
+    let coordenadas = [(i, j) | i <- [0..tamanhoMatriz-1], j <- [0..tamanhoMatriz-1]]
+    in foldr (atualizarRegiao regioesMatriz) regioes coordenadas
+
+atualizarRegiao :: [[Int]] -> (Int, Int) -> [[(Int, Int)]] -> [[(Int, Int)]]
+atualizarRegiao regioesMatriz (i, j) regioes =
+    let idRegiao = regioesMatriz !! i !! j
+        regiaoAtualizada = (i, j) : (regioes !! idRegiao)
+    in take idRegiao regioes ++ [regiaoAtualizada] ++ drop (idRegiao + 1) regioes
+
 main = do
-    strNumber <- getLine
-    let number = (read strNumber :: Int)
-    imprimirMatriz (criarMatriz number)
+    -- imprimirMatriz matrizRegioes
+    print (definirRegioes matrizRegioes quantidadeRegioes tamanhoMatriz)
