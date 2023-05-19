a87e2e34e3ecafa4f7abca540340cc7bed878021
Luan-Diniz <luan.diniz.moraes@hotmail.com>
2023-05-19 18:44:41 -0300

    Codigo feito, falta corrigir erros

--- elixir/kojun.exs
+++ elixir/kojun.exs
@@ -19,34 +19,34 @@ matrizNumerosInicial = [[0, 0, 3, 0, 2, 0, 3, 0, 0, 0, 7, 0, 0, 5, 0, 3, 6],
 
 #Matriz que define as regiões do quebra-cabeça. As regiões devem ser representadas por inteiros de 0 até n,
 #Sendo n a quantidade de regiões - 1. Esses inteiros podem ser considerados o id de cada região.
-#matrizRegioes = [[0 , 1 , 1 , 1 , 1 , 2 , 3 , 4 , 4 , 5 , 6 , 6 , 7 , 7 , 8 , 9 , 9 ],
-#                 [10, 11, 11, 12, 3 , 3 , 3 , 13, 13, 13, 6 , 6 , 6 , 7 , 7 , 9 , 9 ],
-#                 [10, 10, 12, 12, 14, 14, 14, 13, 13, 13, 6 , 6 , 7 , 7 , 9 , 9 , 9 ],
-#                 [15, 15, 16, 16, 16, 14, 14, 17, 17, 18, 18, 18, 18, 19, 19, 20, 20],
-#                 [15, 15, 16, 16, 21, 22, 14, 17, 17, 18, 23, 24, 19, 19, 25, 26, 20],
-#                 [27, 28, 16, 16, 21, 22, 22, 22, 18, 18, 23, 19, 19, 25, 25, 26, 26],
-#                 [29, 28, 28, 29, 21, 30, 30, 22, 31, 23, 23, 32, 19, 25, 25, 33, 34],
-#                 [29, 29, 29, 29, 29, 35, 30, 30, 31, 23, 23, 32, 32, 25, 33, 33, 36],
-#                 [37, 38, 38, 38, 35, 35, 35, 30, 31, 39, 39, 39, 40, 40, 33, 36, 36],
-#                 [37, 37, 38, 41, 41, 35, 35, 30, 43, 39, 39, 39, 44, 40, 36, 36, 36],
-#                 [37, 37, 38, 41, 41, 41, 42, 43, 43, 43, 45, 39, 44, 44, 44, 44, 46],
-#                 [37, 37, 38, 38, 47, 41, 42, 42, 48, 48, 45, 45, 45, 44, 49, 46, 46],
-#                 [50, 51, 51, 52, 47, 41, 42, 42, 54, 48, 48, 45, 45, 49, 49, 49, 49],
-#                 [50, 50, 51, 52, 47, 47, 53, 42, 55, 48, 48, 56, 57, 57, 49, 58, 58],
-#                 [50, 50, 51, 52, 52, 53, 53, 59, 55, 55, 56, 56, 56, 57, 60, 58, 58],
-#                 [50, 50, 61, 61, 52, 53, 53, 59, 55, 55, 56, 62, 62, 57, 60, 60, 63],
-#                 [61, 61, 61, 61, 52, 52, 53, 53, 55, 55, 56, 56, 62, 60, 60, 60, 60]]
-matrizRegioes = [[0,0,1,1,1,2],
-                  [3,3,3,3,3,2],
-                  [4,5,5,5,3,6],
-                  [4,4,4,5,6,6],
-                  [7,7,8,9,9,9],
-                  [10,10,8,8,9,9]]
+matrizRegioes = [[0 , 1 , 1 , 1 , 1 , 2 , 3 , 4 , 4 , 5 , 6 , 6 , 7 , 7 , 8 , 9 , 9 ],
+                 [10, 11, 11, 12, 3 , 3 , 3 , 13, 13, 13, 6 , 6 , 6 , 7 , 7 , 9 , 9 ],
+                 [10, 10, 12, 12, 14, 14, 14, 13, 13, 13, 6 , 6 , 7 , 7 , 9 , 9 , 9 ],
+                 [15, 15, 16, 16, 16, 14, 14, 17, 17, 18, 18, 18, 18, 19, 19, 20, 20],
+                 [15, 15, 16, 16, 21, 22, 14, 17, 17, 18, 23, 24, 19, 19, 25, 26, 20],
+                 [27, 28, 16, 16, 21, 22, 22, 22, 18, 18, 23, 19, 19, 25, 25, 26, 26],
+                 [29, 28, 28, 29, 21, 30, 30, 22, 31, 23, 23, 32, 19, 25, 25, 33, 34],
+                 [29, 29, 29, 29, 29, 35, 30, 30, 31, 23, 23, 32, 32, 25, 33, 33, 36],
+                 [37, 38, 38, 38, 35, 35, 35, 30, 31, 39, 39, 39, 40, 40, 33, 36, 36],
+                 [37, 37, 38, 41, 41, 35, 35, 30, 43, 39, 39, 39, 44, 40, 36, 36, 36],
+                 [37, 37, 38, 41, 41, 41, 42, 43, 43, 43, 45, 39, 44, 44, 44, 44, 46],
+                 [37, 37, 38, 38, 47, 41, 42, 42, 48, 48, 45, 45, 45, 44, 49, 46, 46],
+                 [50, 51, 51, 52, 47, 41, 42, 42, 54, 48, 48, 45, 45, 49, 49, 49, 49],
+                 [50, 50, 51, 52, 47, 47, 53, 42, 55, 48, 48, 56, 57, 57, 49, 58, 58],
+                 [50, 50, 51, 52, 52, 53, 53, 59, 55, 55, 56, 56, 56, 57, 60, 58, 58],
+                 [50, 50, 61, 61, 52, 53, 53, 59, 55, 55, 56, 62, 62, 57, 60, 60, 63],
+                 [61, 61, 61, 61, 52, 52, 53, 53, 55, 55, 56, 56, 62, 60, 60, 60, 60]]
+#matrizRegioes = [[0,0,1,1,1,2],
+#                  [3,3,3,3,3,2],
+#                  [4,5,5,5,3,6],
+#                  [4,4,4,5,6,6],
+#                  [7,7,8,9,9,9],
+#                  [10,10,8,8,9,9]]
 
 
 
-tamanhoMatriz = 6
-#tamanhoMatriz = length(matrizNumerosInicial)
+#tamanhoMatriz = 6
+tamanhoMatriz = length(matrizNumerosInicial)
 
 #Kj de KoJun
 defmodule Kj do
@@ -167,7 +167,7 @@ defmodule Kj do
 
     verificarRegiao(num, regiao, numerosMatriz) and
     verificarAdjacentes(num, i, j, numerosMatriz, tamanhoMatriz) and
-    verificarCimaBAixo(num, i, j, numerosMatriz, regioesMatriz, tamanhoMatriz)
+    verificarCimaBaixo(num, i, j, numerosMatriz, regioesMatriz, tamanhoMatriz)
   end
 
   # Regra 1: uma região precisa ter todos os números de 1 a N sem repetição, sendo N a quantidade de células na região.
@@ -194,34 +194,28 @@ defmodule Kj do
   def verificarCimaBaixo(num, i, j, numerosMatriz, regioesMatriz, tamanhoMatriz) do
     #Verifica se a casa de cima é da mesma região e é menor, que é inválido.
     not (
-
+      ((i-1) >= 0) and
+      ((Enum.at(Enum.at(regioesMatriz, (i-1)), j)) == (Enum.at(Enum.at(regioesMatriz, (i)), j))) and
+      ((Enum.at(Enum.at(numerosMatriz, (i-1)), j)) < num)
     ) and
     #Verifica se a casa de baixo é da mesma região e é maior, que é inválido
     not (
-
+      ((i+1) < tamanhoMatriz) and
+      ((Enum.at(Enum.at(regioesMatriz, (i+1)), j)) == (Enum.at(Enum.at(regioesMatriz, (i)), j))) and
+      (Enum.at(Enum.at(numerosMatriz, (i+1)), j) > num)
     )
 
   end
 
-  """
-    -- Verifica se a casa de cima é da mesma região e é menor, que é inválido
-    not (
-            ((i-1) >= 0) &&
-            ((regioesMatriz !! (i-1) !! j) == (regioesMatriz !! i !! j)) &&
-            ((numerosMatriz !! (i-1) !! j) < num)
-        )
-    &&
-    -- Verifica se a casa de baixo é da mesma região e é maior, que é inválido
-    not (
-            ((i+1) < tamanhoMatriz) &&
-            ((regioesMatriz !! (i+1) !! j) == (regioesMatriz !! i !! j)) &&
-            ((numerosMatriz !! (i+1) !! j) > num)
-        )
-  """
-
 end
 
 
-IO.inspect Kj.definirRegioes(matrizRegioes, Kj.quantidadeRegioes(matrizRegioes), tamanhoMatriz)
 
-#CONTINUAR A PARTIR DA LINHA 184 DO CÓDIGO .hs
+{ehPossivel, matriz} = Kj.kojun(0, 0, matrizNumerosInicial, matrizRegioes,
+                          Kj.definirRegioes(matrizRegioes,Kj.quantidadeRegioes(matrizRegioes), tamanhoMatriz), tamanhoMatriz)
+
+if (ehPossivel) do
+  Kj.imprimirMatriz(matriz)
+else
+  IO.puts "Não há solução!"
+end
